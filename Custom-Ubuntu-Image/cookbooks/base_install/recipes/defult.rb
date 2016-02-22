f86df131efafe74df547f581a154a15406a991ce
Chase Hallsted <chase.hallsted@gmail.com>
2016-02-21 20:18:21 -0600

    Now imports other recipes

--- Custom-Ubuntu-Image/cookbooks/base_install/recipes/defult.rb
+++ Custom-Ubuntu-Image/cookbooks/base_install/recipes/defult.rb
@@ -1,34 +1,24 @@
 #!/usr/bin/env ruby
-# TODO: Seperate this into individual packages and call them from here
-# Change this value to a user you want to own everything
-user = vagrant
 
-case node['platform_family']
-when ubuntu debian mint do
-  apt_update "apt-get update" do
-      action :update
-  end
-end
-
-package curl do
-    action :upgrade
-end
-package wget do
-    action :upgrade
-end
-package zsh do
-    action :upgrade
-end
-
-user #{user} do
+user "#{user}" do
     comment "Managed by Chef"
     home "/home/#{user}"
     shell '/usr/bin/zsh'
     action :create
 end
 
+include_recipe 'git'
+
 git 'Chases Dotfiles' do
     repository 'git://github.com/challsted/dotfiles.git'
     destination "/home/#{user}/git/dotfiles"
     action :sync
 end
+
+include_recipe 'vim'
+# include_recipe 'nvim' # Once the recipe is fixed this canb e uncommented
+include_recipe 'zsh'
+include_recipe 'tmux'
+include_recipe 'ruby'
+include_recipe 'python'
+
